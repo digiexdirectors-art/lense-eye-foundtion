48c48600df7c0b20395319e89f1075af8eecadaf
rajdeep-adhikary-codes <rajdeepadhikary@protonmail.com>
2026-04-14 15:56:57 +0530

    updated frontend API url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://213.210.36.24:5000',
         changeOrigin: true,
       },
     },
